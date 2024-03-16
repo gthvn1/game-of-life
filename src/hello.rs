9d35cae94d117738c54fe360849f0136978b62ed
Guillaume <guillaume.thouvenin@vates.tech>
2024-03-16 17:28:47 +0100

    link with sayHello

--- src/hello.rs
+++ src/hello.rs
@@ -1,9 +1,18 @@
-use std::os::raw::c_int;
+use std::{
+    ffi::{c_void, CString},
+    os::raw::{c_char, c_int},
+};
 
 extern "C" {
     fn getNumber() -> c_int;
+    fn sayHello(name: *const c_char) -> c_void;
 }
 
 pub fn get_number() -> i32 {
-    return unsafe { getNumber() };
+    unsafe { getNumber() }
+}
+
+pub fn say_hello(name: String) {
+    let c_name = CString::new(name).unwrap();
+    unsafe { sayHello(c_name.as_ptr() as *const c_char) };
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod hello;
 fn main() {
     let x: i32 = hello::get_number();
     println!("Got {x}!");
+    hello::say_hello("Sailor".to_string());
 }
